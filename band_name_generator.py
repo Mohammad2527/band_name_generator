59023eedfe47eb8a82d973c7cd3ecaacef362f0c
Mohammad Kawshick <63961452+Mohammad2527@users.noreply.github.com>
2020-11-25 00:44:12 +1100

    source code
    source code for band_name_generator

---
+++ band_name_generator.py
@@ -0,0 +1,6 @@
+print("Welcome to band name generator!")
+
+city_name = input("Where did you grew up?\n")
+pet_name = input("What is your pet name?\n")
+
+print("Your band name could be: " + city_name + ' ' + pet_name)
